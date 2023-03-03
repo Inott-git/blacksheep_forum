99583fb00d483a9237c9145aafc32e583a987310
inott-git <arnst.artyom@gmail.com>
2023-03-03 19:30:38 +0300

    add_com(2.0)

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -6,19 +6,14 @@ from db import db_connect
 
 
 class Admin(Controller):
-    @get('/admin')
-    async def adminchik(self, request: Request):
-        check = auth.handle(request)
+    @get('/add_post')
+    async def addpost(self, request: Request):
         category = db_connect.Categories.get_all_categories()
         category = list(map(list, category))
+        return self.view('addpost.html', {'category': category} )
 
-        if check == True:
-            return self.view('addpost.html', {'category': category} )
-        else:
-            return 'You are not admin'
-
-    @post('/admin')
-    async def admin(self, request: Request):
+    @post('/add_post')
+    async def add_post(self, request: Request):
         data = await request.form()
         identity = request.cookies
         id_user = db_connect.Users.get_id(identity['identity'])
@@ -26,3 +21,17 @@ class Admin(Controller):
         db_connect.Posts.add_post(user_id=id_user, title=data['title'], description=data['description'], cat_id=cat_id)
         respounse = self.redirect('/')
         return respounse
+
+    @get('/add_cat')
+    async def addcat(self, request: Request):
+        check = auth.handle(request)
+        if check:
+            return self.view('addcat.html')
+        else:
+            return 'You are not admin'
+
+    @post('/add_cat')
+    async def add_cat(self, request: Request):
+        data = await request.form()
+        db_connect.Categories.add_category(data['title'])
+        return self.redirect('/')
